23e14d7ca84725bb7933504d4f86bcc6c28e0768
Evian-Chen <paramecium128@gmail.com>
2025-02-04 17:28:28 +0800

    feat: update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Link href="/chat">yooooooooooo</Link>
         <Image
           className={styles.logo}
           src="/next.svg"
@@ -87,7 +89,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          Go to nextjs.org ???
         </a>
       </footer>
     </div>
